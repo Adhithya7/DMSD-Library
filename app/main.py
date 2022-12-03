950728dedde7cf4cadd59de94083e40fc0d47924
Adhithya7 <athironaldo@gmail.com>
2022-12-03 17:55:01 -0500

    Added db_utils

---
+++ app/main.py
@@ -0,0 +1,15 @@
+transactions
+
+search (view single) - GET /library/transactions?id=5
+list all - GET /library/transactions
+create - POST /library/transactions
+update - PUT /library/transactions?id=5
+delete - DELETE /library/transactions?id=5
+
+GET
+POST
+PATCH
+PUT
+DELETE
+
+DELETE /library/transactions?id=5
