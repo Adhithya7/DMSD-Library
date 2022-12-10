0091aadc4d68c8a9624b1e60f63bf56d1c737fef
Adhithya7 <athironaldo@gmail.com>
2022-12-09 23:33:29 -0500

    Pushed search change

--- app/views/reader.py
+++ app/views/reader.py
@@ -6,25 +6,28 @@ reader = Blueprint('reader', __name__, url_prefix='/reader')
 
 @reader.route("/search", methods=["GET"])
 def search():
-    query = """ SELECT * from document D 
+    all_docs_query = """ SELECT * from document D 
                 JOIN PUBLISHER P 
                 ON D.publisherid = P.publisherid 
             """
     if request.args.get('docid'):
-        query += f"and D.docid={request.args.get('docid')}"
+        all_docs_query += f"and D.docid={request.args.get('docid')}"
     if request.args.get("title"):
-        query += f" and D.title like %{request.args.get('title')}%"
+        all_docs_query += f" and D.title like %{request.args.get('title')}%"
     if request.args.get("publisher_name"):
-        query += f" and P.publisher_name like %{request.args.get('publisher_name')}%"
+        all_docs_query += f" and P.publisher_name like %{request.args.get('publisher_name')}%"
     ql = int(request.args.get('limit', 10))
     if ql < 1 or ql > 100:
-        query += f" LIMIT 10"
+        all_docs_query += f" LIMIT 10"
     else:
-        query += f" LIMIT {ql}"
-    print(query)
-    cursor.execute(query)
-    rows = cursor.fetchall()
-    return render_template("index.html", rows=rows)
+        all_docs_query += f" LIMIT {ql}"
+    print(all_docs_query)
+    cursor.execute(all_docs_query)
+    all_docs = set(cursor.fetchall())
+    doc_ids = [row[0] for row in all_docs]
+
+
+    return render_template("index.html", rows=all_docs)
 
 @reader.route("/document/{id}", methods=["GET", "POST", "PUT"])
 def document():
