781ebb331548997c99cda30c1d055993a012ecb2
sj749@njit.edu <sj749@njit.edu>
2022-12-13 04:34:14 -0500

    fixed bugs and alignments.

--- app/views/reader.py
+++ app/views/reader.py
@@ -2,9 +2,10 @@ import traceback as tb
 from flask import Blueprint, render_template, request, flash, redirect, url_for, make_response
 from utils import connection, cursor
 
-reader = Blueprint('reader', __name__, url_prefix='/reader', template_folder='templates')
+reader = Blueprint('reader', __name__, url_prefix='/reader',
+                   template_folder='templates')
 
-available_query = """ 
+available_query = """
                 SELECT C.docid, C.copyno, C.bid from copy C
                 EXCEPT
                 ((SELECT C.docid, C.copyno, C.bid from copy C
@@ -26,14 +27,16 @@ available_query = """
                 on C.docid = B.docid))
     """
 
-docs_query = """ SELECT docid, title, pdate, D.publisherid as publisherid, pubname,address from document D 
-                JOIN PUBLISHER P 
+docs_query = """ SELECT docid, title, pdate, D.publisherid as publisherid, pubname,address from document D
+                JOIN PUBLISHER P
                 ON D.publisherid = P.publisherid
             """
+
 
 @reader.route("/", methods=["GET"])
 def base():
     return render_template("home.html")
+
 
 @reader.route("/validate", methods=["GET"])
 def validate():
@@ -46,11 +49,13 @@ def validate():
     flash("Please register with the library first.", "danger")
     return render_template("home.html", rid=rid)
 
+
 @reader.route("/search", methods=["GET"])
 def search():
     rid = request.args.get('rid')
     all_docs_query = docs_query
-    cursor.execute(f'SELECT sub.docid from ({available_query.format(rid=rid)}) AS sub')
+    cursor.execute(
+        f'SELECT sub.docid from ({available_query.format(rid=rid)}) AS sub')
     available_docs = cursor.fetchall()
     available_docs = set([row[0] for row in available_docs])
     # print(available_docs)
@@ -73,21 +78,23 @@ def search():
     rows = []
     for row in all_docs:
         tmp = list(row)
-        tmp.append(str(request.args.get("available") or row[0] in available_docs))
+        tmp.append(str(request.args.get("available")
+                   or row[0] in available_docs))
         rows.append(tmp)
     rows.insert(0, columns)
     # print(rows)
     return render_template("explore.html", rows=rows, rid=rid)
 
+
 @reader.route("/document/<id>", methods=["GET", "POST", "PUT", "DELETE"])
 def document(id):
     all_docs_query = docs_query
     if request.method == "GET":  # View document
-        available=request.args.get('available')
+        available = request.args.get('available')
         doc_query = "select {cols} from {type} where docid={id}"
 
         for type in ['book', 'journal_volume', 'proceedings']:
-            cursor.execute(doc_query.format(cols= 'docid',type=type, id=id))
+            cursor.execute(doc_query.format(cols='docid', type=type, id=id))
             if cursor.fetchall():
                 doc_type = type
 
@@ -97,7 +104,7 @@ def document(id):
             spe_query = f"""select p.docid as docid, isbn ,pname from ({person_query}) as p
                             join book b on p.docid = b.docid"""
             final_query = f"""select D.docid as docid, title, pdate, D.publisherid as publisherid,
-                            pubname, address, isbn, pname 
+                            pubname, address, isbn, pname
                             from ({all_docs_query} and D.docid={id}) as D
                             left join ({spe_query}) as S on D.docid = S.docid"""
 
@@ -131,16 +138,17 @@ def document(id):
         print(rows)
         return render_template("resource.html", rows=rows, doc_type=doc_type, available=available)
 
-    elif request.method == "POST": # RESERVE DOC
+    elif request.method == "POST":  # RESERVE DOC
         rid = request.args.get('rid')
-        available=request.args.get('available')
-        #select a copy
+        available = request.args.get('available')
+        # select a copy
         copy_query = f"""SELECT D.copyno, D.bid from ({available_query.format(rid=rid)}) AS D
                         WHERE docid={id} limit 1;"""
         cursor.execute(copy_query)
         copy = cursor.fetchall()
         if not copy:
-            flash("User has already reserved/borrowed a different copy of the book", "danger")
+            flash(
+                "User has already reserved/borrowed a different copy of the book", "danger")
             print("User has already reserved/borrowed a different copy of the book")
             return ()
         resv_query = f"""INSERT INTO RESERVES (RID, DOCID, COPYNO, BID) VALUES
@@ -154,7 +162,7 @@ def document(id):
             flash(f"Unexpected error while reserving book: {e}")
         return redirect(request.url)
 
-    elif request.method == "PUT": # BORROW DOC
+    elif request.method == "PUT":  # BORROW DOC
         rid = request.args.get('rid')
         copy_query = f"""SELECT copyno, bid from reserves
                         WHERE docid = {id} and rid = {rid};"""
@@ -162,30 +170,52 @@ def document(id):
         copy = cursor.fetchall()
         borrow_query = f"""INSERT INTO BORROWS (RID, DOCID, COPYNO, BID) VALUES
                         ({rid}, {id}, {copy[0][0]}, {copy[0][1]});"""
-        try:                        
+        try:
             cursor.execute(borrow_query)
             connection.commit()
-            flash("Borrowed Book", "success")         
+            flash("Borrowed Book", "success")
         except Exception as e:
             print(tb.format_exc())
             flash(f"Unexpected error while Borrowing book: {e}")
         return redirect(request.url)
 
-    elif request.method == "DELETE":  # RETURN DOC
-        rid = request.args.get('rid')
-        borrow_query = f"""SELECT BOR_NO from BORROWS
+
+@reader.route("/return_document/<id>", methods=["GET"])
+def return_document(id):
+    rid = request.args.get('rid')
+    borrow_query = f"""SELECT BOR_NO from BORROWS
                         WHERE docid = {id} and rid = {rid}"""
-        return_query = f"""UPDATE BORROWING
+    return_query = f"""UPDATE BORROWING
                             SET RDTIME = now()
                             WHERE BOR_NO in ({borrow_query}) and RDTIME is NULL;"""
-        try:
-            cursor.execute(return_query)
-            connection.commit()
-            flash("Returned Book", "success") 
-        except Exception as e:
-            print(tb.format_exc())
-            flash(f"Unexpected error while Returning book: {e}")
-        return redirect(request.url)
+    try:
+        cursor.execute(return_query)
+        connection.commit()
+        # flash("Returned Book", "success")
+    except Exception as e:
+        print(tb.format_exc())
+        flash(f"Unexpected error while Returning book: {e}")
+        
+    #Loading my transactions page again
+    reserve_query = f"""SELECT docid, bid, dtime as tx_time, NULL as ret_time, 'RESERVED' as status  from RESERVES R
+                        JOIN RESERVATION RI ON R.reservation_no = RI.res_no and R.rid = {rid}"""
+    borrow_query = f"""SELECT docid, bid, bdtime as tx_time, rdtime as ret_time, 'RETURNED' as STATUS from BORROWS B
+                      JOIN BORROWING BI ON B.bor_no = BI.bor_no and B.rid = {rid} and BI.rdtime is not null"""
+    return_query = f"""SELECT docid, bid, bdtime as tx_time, NULL as ret_time, 'BORROWED' as STATUS from BORROWS B
+                       JOIN BORROWING BI ON B.bor_no = BI.bor_no and B.rid = {rid} and BI.rdtime is null"""
+    docs = f"""  SELECT sub1.docid as "Doc ID", title as "Title", location as "BranchLocation", tx_time as "Borrowed On", ret_time as "Returned On", status as "Status" from 
+                (({reserve_query}) UNION ({borrow_query}) UNION ({return_query})) as sub1
+                JOIN DOCUMENT D on sub1.docid = D.docid
+                JOIN BRANCH B ON sub1.bid = B.bid
+                """
+
+    cursor.execute(docs)
+    rows = cursor.fetchall()
+    columns = [desc[0] for desc in cursor.description]
+    rows.insert(0, columns)
+    print(rows)
+    return render_template("my_transactions.html", rows=rows, rid=rid)
+
 
 @reader.route("/document", methods=["GET"])
 def list_documents():
@@ -201,7 +231,7 @@ def list_documents():
                 JOIN DOCUMENT D on sub1.docid = D.docid
                 JOIN BRANCH B ON sub1.bid = B.bid
                 """
-                
+
     cursor.execute(docs)
     rows = cursor.fetchall()
     columns = [desc[0] for desc in cursor.description]
