7faf20c496dd0c0d4ce65b30fe8155b358abcb1f
Adhithya7 <athironaldo@gmail.com>
2022-12-13 20:05:34 -0500

    Changed field type in copy table

--- app/utils/raw_sql/000_tables.sql
+++ app/utils/raw_sql/000_tables.sql
@@ -114,7 +114,7 @@ CREATE TABLE
 CREATE TABLE
     COPY(
         DOCID int,
-        COPYNO int,
+        COPYNO serial,
         BID int,
         POSITION VARCHAR(30),
         FOREIGN KEY (DOCID) REFERENCES DOCUMENT(DOCID),
