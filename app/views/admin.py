92e59e27dfffce71fcceb25cb767936d49159d3e
sj749@njit.edu <sj749@njit.edu>
2022-12-14 00:43:23 -0500

    7 apis done.

--- app/views/admin.py
+++ app/views/admin.py
@@ -14,8 +14,51 @@ admin = Blueprint('admin', __name__, url_prefix='/admin',
 def home_page():
     return render_template('adminHome.html')
 
-@admin.route("/most_borrowed", methods=["GET"])
+@admin.route("/most_borrowed", methods=["GET"]) # most borrowed books
 def most_borrowed():
     n_value = request.args.get('n_value')
     print(n_value)
+    return render_template('contact.html')
+
+@admin.route("/most_popular", methods=["GET"]) # most popular books of a year
+def most_popular():
+    year_value = request.args.get('year_value')
+    print(year_value)
+    return render_template('contact.html')
+
+@admin.route("/avg_fine", methods=["GET"]) # average fine paid by the borrowers by branch in a time period
+def avg_fine():
+    st_value = request.args.get('st_value')
+    print(st_value)
+    end_value = request.args.get('end_value')
+    print(end_value)
+    return render_template('contact.html')
+
+@admin.route("/most_borrowed_in_branch", methods=["GET"]) # Get the N most borrowed books in branch I.
+def most_borrowed_in_branch():
+    n_value = request.args.get('n_value')
+    print(n_value)
+    i_value = request.args.get('i_value')
+    print(i_value)
+    return render_template('contact.html')
+
+# Get the top N most frequent borrowers in the library and the number of books borrowed.
+@admin.route("/freq_borrowers", methods=["GET"])
+def freq_borrowers():
+    n_value = request.args.get('n_value')
+    print(n_value)
+    return render_template('contact.html')
+
+@admin.route("/api_5", methods=["GET"]) # Get the N most borrowed books in branch I.
+def api_5():
+    n_value = request.args.get('n_value')
+    print(n_value)
+    i_value = request.args.get('i_value')
+    print(i_value)
+    return render_template('contact.html')
+
+@admin.route("/api_4", methods=["GET"]) # Print branch information (name and location). 
+def api_4():
+    bid_value = request.args.get('bid_value')
+    print(bid_value)
     return render_template('contact.html')
