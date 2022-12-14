fcc114da5f0cc2d2392cb026e09ee15f53992388
sj749@njit.edu <sj749@njit.edu>
2022-12-14 00:12:49 -0500

    made first makeshift admin api

--- app/views/admin.py
+++ app/views/admin.py
@@ -3,8 +3,19 @@ import csv
 import traceback
 from flask import Blueprint, render_template, request, redirect, flash
 
-admin = Blueprint('admin', __name__, url_prefix='/admin')
+admin = Blueprint('admin', __name__, url_prefix='/admin',
+                   template_folder='templates')
 
-@admin.route("/import", methods=["GET","POST"])
-def document():
-    pass
+# @admin.route("/import", methods=["GET","POST"])
+# def document():
+#     pass
+
+@admin.route("/home_page", methods=["GET"])
+def home_page():
+    return render_template('adminHome.html')
+
+@admin.route("/most_borrowed", methods=["GET"])
+def most_borrowed():
+    n_value = request.args.get('n_value')
+    print(n_value)
+    return render_template('contact.html')
